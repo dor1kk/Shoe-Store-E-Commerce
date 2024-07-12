75acdf0c1aade8094801b6ee18fbabe2d856952b
dor1kk <dorajetkukaj@gmail.com>
2024-07-12 15:57:40 +0500

    project setup

---
+++ Shoe-Shop/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Navbar from './assets/components/Navbar'
+
+const App = () =>(
+    <main className='relative'>
+      Navbar
+      <section className='padding'>
+        Hero
+      </section>
+      <section className='padding'>
+        Popular Products
+      </section>
+      <section className='padding'>
+        SuperQuality
+      </section>
+      <section className='padding-x py-10'>
+        Services
+      </section>
+      <section className='padding'>
+        SpecialOffers
+      </section>
+      <section className='padding bg-pale-blue'>
+          CustomerReviews
+        </section>
+      <section className='padding'>
+          Subsctribe
+        </section>
+        <section className='padding'>
+        Footer
+      </section>      
+    </main>
+  )
+
+
+export default App
---
+++ Shoe-Shop/src/assets/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <ul>
+            Navbar
+
+        </ul>
+      
+    </div>
+  )
+}
+
+export default Navbar
